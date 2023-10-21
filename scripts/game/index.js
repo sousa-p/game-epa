2ed5c9bb3f975cf0337f6076e4a01ef62cf4505f
RomuuloGoncalves <romulogoncalves13@gmail.com>
2023-10-21 20:30:18 -0300

    sprites luvas

--- scripts/game/index.js
+++ scripts/game/index.js
@@ -1,2 +1,10 @@
+import { addButton } from "../utils/btn.js";
 scene("game", () => {
+
+    onUpdate(() => setCursor("default"));
+
+    addButton("esquerda", vec2(106.25, 360), () => go("game"), [212.5, 720], [255,0,0], 0, [255, 255, 255], 28, [255, 255, 255]);
+    addButton("direita", vec2(317, 360), () => go("game"), [212.5, 720], [0, 0, 255], 0, [255, 255, 255], 28, [255, 255, 255]);
+
 });
+go("menu");
--- scripts/gloves/index.js
+++ scripts/gloves/index.js
@@ -1,39 +1,69 @@
 import { addButton } from "../utils/btn.js";
+loadSprite("luva-1.1", "../../assets/sprts/gloves/default-gloves/glove01-left-sprite.png")
+loadSprite("luva-1.2", "../../assets/sprts/gloves/default-gloves/glove01-right-sprite.png")
 
-loadSprite("luva-1", "../../assets/sprts/gloves/luva-1.png")
-loadSprite("luva-2", "../../assets/sprts/gloves/luva-2.png")
-loadSprite("luva-3", "../../assets/sprts/gloves/luva-3.png")
+loadSprite("luva-2.1", "../../assets/sprts/gloves/classic-gloves/glove02-left-sprite.png")
+loadSprite("luva-2.2", "../../assets/sprts/gloves/classic-gloves/glove02-right-sprite.png")
 
-const gloveSprites = ["luva-1", "luva-2", "luva-3"];
-const nomeGloves = ["luva um", "luva dois", "luva tres"];
-let gloveAtual = 0;
-let gloves;
-let nomeGlove
+loadSprite("luva-3.1", "../../assets/sprts/gloves/vip-gloves/glove03-left-sprite.png")
+loadSprite("luva-3.2", "../../assets/sprts/gloves/vip-gloves/glove03-right-sprite.png")
+
+const glovesData = {
+    "luva-1": [
+        { sprite: "luva-1.1", nome: "Luva Padrao" },
+        { sprite: "luva-1.2", nome: "luva Padrao2" }
+    ],
+    "luva-2": [
+        { sprite: "luva-2.1", nome: "luva Classica" },
+        { sprite: "luva-2.2", nome: "luva Classica" }
+    ],
+    "luva-3": [
+        { sprite: "luva-3.1", nome: "luva Vip" },
+        { sprite: "luva-3.2", nome: "luva Vip" }
+    ]
+};
+
+let indexLuva = 0;
+let luva_esquerda;
+let luva_direita;
+let nome_luva;
 
 function changeGlove(acao) {
-    gloveAtual += acao;
+    indexLuva += acao;
 
-    if (gloveAtual < 0) {
-        gloveAtual = gloveSprites.length - 1;
-    } else if (gloveAtual >= gloveSprites.length) {
-        gloveAtual = 0;
+    if (indexLuva < 0) {
+        indexLuva = Object.keys(glovesData).length - 1;
+    } else if (indexLuva >= Object.keys(glovesData).length) {
+        indexLuva = 0;
     }
 
-    if (gloves || nomeGlove) {
-        destroy(gloves);
-        destroy(nomeGlove);
+    if (luva_esquerda || luva_direita || nome_luva) {
+        destroy(luva_esquerda);
+        destroy(luva_direita);
+        destroy(nome_luva);
     }
 
-    gloves = add([
-        sprite(gloveSprites[gloveAtual]),
-        pos(center().x - 100, center().y - 150),
+    const luvaAtual = Object.keys(glovesData)[indexLuva];
+    const luvas = glovesData[luvaAtual];
+
+    luva_esquerda = add([
+        sprite(luvas[0].sprite),
+        pos(center().x - 90, center().y - 100),
         outline(4),
+        scale(0.1),
     ]);
 
-    nomeGlove = add([
-        text(nomeGloves[gloveAtual]),
+    luva_direita = add([
+        sprite(luvas[1].sprite),
+        pos(center().x, center().y - 100),
+        outline(4),
+        scale(0.1),
+    ]);
+
+    nome_luva = add([
+        text(luvas[0].nome),
         pos(center().x / 2, 130),
-        scale(1.5),
+        scale(1.1),
         color(80, 242, 242),
     ]);
 
@@ -53,7 +83,7 @@ scene("gloves", () => {
         rect(200, 200),
         outline(1),
         area(),
-    ])
+    ]);
 
     changeGlove(0);
     addButton("Ant", vec2(55, 300), () => changeGlove(-1), [85, 40]);
