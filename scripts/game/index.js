eb673908bcd40a56d4baecf775a72ef625e3f71b
sousa-p <p.soousa7@gmail.com>
2023-10-22 23:42:48 -0300

    Feature inimigos aleatorios e luvas

--- scripts/game/index.js
+++ scripts/game/index.js
@@ -1,10 +1,8 @@
 import { addButton } from "../utils/btn.js";
 
 loadSound("count", "../../assets/audio/gameplay/count.mp3")
-loadSprite("nerdyDeath", "../../assets/sprts/enemy/nerdy/expressions/nerdy-death.png");
-loadSprite("nerdyDefault", "../../assets/sprts/enemy/nerdy/expressions/nerdy-default.png");
-loadSprite("nerdyDemageLeft", "../../assets/sprts/enemy/nerdy/expressions/nerdy-demage-left.png");
-loadSprite("nerdyDemageRight", "../../assets/sprts/enemy/nerdy/expressions/nerdy-demage-right.png");
+loadSprite("enemyLeftGlove", "../../assets/sprts/enemy/gloves/enemy-left-gloves.png")
+loadSprite("enemyRightGlove", "../../assets/sprts/enemy/gloves/enemy-right-gloves.png")
 
 
 scene("gameStart", () => {
@@ -28,7 +26,6 @@ scene("gameStart", () => {
         });
     });
     wait(count.length, () => {
-        window.GAME = undefined;
         go("onGame");
     })
 });
@@ -36,25 +33,33 @@ scene("gameStart", () => {
 scene("onGame", () => {
     if (!window.GAME) {
         const musicSelected = Math.round(Math.random() + 1);
-        
+        const enemies = ['nerdy', 'bigas'];
+
         window.GAME = {
             SPEED: 1,
             POINTS: 0,
+            ENEMY: enemies[Math.floor(Math.random() * enemies.length)]
         };
-        
+
+        loadSprite("enemyDeath", `../../assets/sprts/enemy/${window.GAME.ENEMY}/expressions/${window.GAME.ENEMY}-death.png`);
+        loadSprite("enemyDefault", `../../assets/sprts/enemy/${window.GAME.ENEMY}/expressions/${window.GAME.ENEMY}-default.png`);
+        loadSprite("enemyDamageLeft", `../../assets/sprts/enemy/${window.GAME.ENEMY}/expressions/${window.GAME.ENEMY}-damage-left.png`);
+        loadSprite("enemyDamageRight", `../../assets/sprts/enemy/${window.GAME.ENEMY}/expressions/${window.GAME.ENEMY}-damage-right.png`);
+
         window.GAME.GLOVE = JSON.parse(localStorage.getItem("glove")) || { name: "Common", type: "default", id: 0 };
+
         
         loadSprite("gloveLeft", `../../assets/sprts/gloves/${window.GAME.GLOVE.type}-gloves/${window.GAME.GLOVE.type}-left.png`);
         loadSprite("gloveRight", `../../assets/sprts/gloves/${window.GAME.GLOVE.type}-gloves/${window.GAME.GLOVE.type}-right.png`);
         loadSound("music", `../../assets/audio/gameplay/music-${musicSelected}.mp3`);
-        
+
         window.GAME.MUSIC = play("music", {
             loop: true,
         });
     } else {
         window.GAME.MUSIC.paused = false;
     }
-    
+
     const leftBtn = add([
         rect(212.5, 750),
         area(),
@@ -74,7 +79,7 @@ scene("onGame", () => {
         pos(vec2(317, 360)),
         color(BLACK)
     ]);
-    
+
     const gloveLeft = add([
         sprite("gloveLeft"),
         scale(.2),
@@ -83,7 +88,7 @@ scene("onGame", () => {
         timer(),
         z(50)
     ]);
-    
+
     const gloveRight = add([
         sprite("gloveRight"),
         scale(.2),
@@ -94,7 +99,7 @@ scene("onGame", () => {
     ]);
 
     const enemyBody = add([
-        sprite("nerdyDefault"),
+        sprite("enemyDefault"),
         scale(.2),
         pos(center()),
         anchor("center"),
@@ -102,9 +107,9 @@ scene("onGame", () => {
     ]);
 
     function enemyDamage(dir) {
-        enemyBody.use(sprite(`nerdyDemage${dir}`));
+        enemyBody.use(sprite(`enemyDamage${dir}`));
         enemyBody.wait(1, () => {
-            enemyBody.use(sprite("nerdyDefault"));
+            enemyBody.use(sprite("enemyDefault"));
         });
     }
 
@@ -113,7 +118,7 @@ scene("onGame", () => {
         gloveLeft.tween(gloveLeft.pos, center(), window.GAME.GLOVE.speed, (p) => gloveLeft.pos = p, easings.easeOutBounce)
         wait(window.GAME.GLOVE.speed, () => {
             enemyDamage('Left');
-            gloveLeft.tween(gloveLeft.pos, vec2(center().x - 100, 620), window.GAME.GLOVE.speed/2, (p) => gloveLeft.pos = p)
+            gloveLeft.tween(gloveLeft.pos, vec2(center().x - 100, 620), window.GAME.GLOVE.speed / 2, (p) => gloveLeft.pos = p)
         });
     });
 
@@ -125,28 +130,28 @@ scene("onGame", () => {
             gloveRight.tween(gloveRight.pos, vec2(center().x + 100, 620), window.GAME.GLOVE.speed / 2, (p) => gloveRight.pos = p)
         });
     });
-    
+
     const enemyLeftGlove = add([
-        circle(55),
-        pos(center().x - 100, center().y + 25),
-        color(RED),
-        anchor("center")
+        sprite("enemyLeftGlove"),
+        pos(center().x - 100, center().y + 50),
+        anchor("center"),
+        scale(.8)
     ]);
 
     const enemyRightGlove = add([
-        circle(55),
-        pos(center().x + 100, center().y + 25),
-        color(RED),
-        anchor("center")
+        sprite("enemyRightGlove"),
+        pos(center().x + 100, center().y + 50),
+        anchor("center"),
+        scale(.8)
     ])
-    
+
     const pause = add([
         text("PAUSE"),
         color(119, 74, 217),
         pos(300, 130),
         area()
     ]);
-    
+
     pause.onClick(() => {
         go("pause");
     })
@@ -155,11 +160,14 @@ scene("onGame", () => {
 scene("pause", () => {
     window.GAME.MUSIC.paused = true;
 
-    
+
     addButton("CONTINUE", vec2(center().x, center().y), () => go("gameStart"));
-    
-    addButton("GIVE UP", vec2(center().x, center().y + 100), () => go("menu"));
-    
+
+    addButton("GIVE UP", vec2(center().x, center().y + 100), () => {
+        window.GAME = undefined;
+        go("menu");
+    });
+
     add([
         text("PAUSED", {
             size: 64
